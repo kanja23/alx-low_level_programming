dc94f0e3d6f1e84091d52e4ce79a05289be07eaf
kanja23 <martinkaranja92@gmail.com>
2022-12-09 02:24:40 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+
+/**
+ *  * main - prints the phrase with proper grammar,
+ *   * but the outcome is a piece of art,
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *     */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
