3472b6d5f3a313dff511cf4bbcb5b3f2be92b3f0
kanja23 <martinkaranja92@gmail.com>
2022-12-19 07:08:51 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+	void swap_int(int *a, int *b)
 	int _strlen(char *s);
 	void _puts(char *str);
 	void print_rev(char *s);
@@ -9,4 +9,5 @@ void swap_int(int *a, int *b)
 	void print_array(int *a, int n);
 	char *_strcpy(char *dest, char *src);
 	int _atoi(char *s);
+
 
