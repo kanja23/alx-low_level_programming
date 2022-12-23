45a736ac9f549143f0f5afe22593c3939780770e
kanja23 <martinkaranja92@gmail.com>
2022-12-23 05:48:13 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,27 +8,16 @@
 
 char *string_toupper(char *word)
 {
-
 	int length;
 
 	length = 0;
-
 	while (word[length] != '\0')
-
 	{
-
 	if (word[length] >= 97 && word[length] <= 122)
-
-		{
-
-			word[length] = word[length] - 32;
-
-		}
-
-	length++;
-
+	{
+	word[length] = word[length] - 32;
 	}
-
+	length++;
+	}
 	return (word);
-
 }
