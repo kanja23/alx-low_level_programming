e4992ee44773d0b8f12dba93a8f1f1fa2e4240bb
kanja23 <martinkaranja92@gmail.com>
2023-01-05 01:37:54 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,14 @@
  * @s: string
  * Return: no return.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-		{
-			_putchar(*s);
-			_puts_recursion(s + 1);
-		}
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
