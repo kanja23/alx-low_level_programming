a301093f192983c61b867aee73432a587055bbc3
kanja23 <martinkaranja92@gmail.com>
2023-01-25 06:39:42 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -82,15 +82,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 	free(dog->name);
 	free(dog);
-																	return (NULL);
-																	}
-
+	return (NULL);
+	}
 	dog->name = _strcopy(dog->name, name);
-
 	dog->age = age;
-
 	dog->owner = _strcopy(dog->owner, owner);
-
 	return (dog);
-
 }
