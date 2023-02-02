c9043213bf8465c5b933a39cbe94066a06bbcb85
kanja23 <martinkaranja92@gmail.com>
2023-02-02 11:05:10 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,6 +7,7 @@
  * @n: n element.
  * Return: address of the new element. NUll if it failed.
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -19,20 +20,20 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-		new->n = n;
-		new->next = NULL;
-		temp = *head;
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
 	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-	while (temp->next != NULL)
 		{
-			temp = temp->next;
+			*head = new;
 		}
-	temp->next = new;
+		else
+	{
+		while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+		temp->next = new;
 	}
 
 	return (*head);
